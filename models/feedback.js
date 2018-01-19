d57b9ea328d9df1db7d1b285aebaf87dc8c4444f
alicialycan <alicialycan@gmail.com>
2018-01-18 18:39:58 -0800

    test

--- models/feedback.js
+++ models/feedback.js
@@ -4,19 +4,19 @@ var app = app || {};
 
 (module => {
 
-    const _API_URL_ = 'http://localhost:3000';
+  const _API_URL_ = 'http://localhost:3000';
 
-    function Feedback() {}
+  function Feedback() {}
 
-    function errorCalback(err) {
-        console.error(err);
-        // app.errorView.initErrorPage(err);
-    }
+  function errorCalback(err) {
+    console.error(err);
+  // app.errorView.initErrorPage(err);
+  }
 
-    Feedback.fetchAll = () => $.getJSON(_API_URL_ + '/api/v1/feedback').done(function( json ) {
-        console.log( "JSON Data: ", json);
-    })
-        .catch(errorCalback);
+  Feedback.fetchAll = () => $.getJSON(_API_URL_ + '/api/v1/feedback').done(function (json) {
+    console.log("JSON Data: ", json);
+  })
+    .catch(errorCalback);
 
-    module.Feedback = Feedback;
+  module.Feedback = Feedback;
 })(app);
