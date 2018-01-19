dbc56e621e41176c1f0747f09a74368027354d1c
alicialycan <alicialycan@gmail.com>
2018-01-19 08:56:52 -0800

    add server communication to client side

--- models/location.js
+++ models/location.js
@@ -4,21 +4,60 @@ var app = app || {};
 
 (module => {
 
-  const __API_URL__ = 'http://www.nwac.us/api/v2/avalanche-forecast/?format=json';
+  const __API_URL__ = 'http://www.nwac.us/api/v2/';
 
-  function Forecast(region, weather, avDanger, avProblems) {
-    this.region = region,
-    this.weather = weather,
-    this.avDanger = avDanger,
-    this.avProblems = avProblems;
+  function avalancheForecast() {}
+  //   avalancheDay1Date = [];
 
-    Forecast.all [];
+  //   function errorCallback(err) {
+  //     console.error(err);
+  // app.errorView.initErrorPage(err);
+  //}
 
-    Forecast.fetchAll = () => $.getJSON(__API_URL__)
+  //     function avalancheForecast(region, weather, avDanger, avProblems) {
+  //         this.region = region,
+  //             this.weather = weather,
+  //             this.avDanger = avDanger,
+  //             this.avProblems = avProblems;
 
-  }
-});
-let location = 'Olympics'
-function findLocation(location){
-    `http://www.nwac.us/api/v2/avalanche-region-forecast/?format=json&zone=${location}`
-}
+  //         avalancheForecast.fetchAll = () => $.getJSON(__API_URL__)
+
+  //     }
+  // });
+
+  avalancheForecast.getForecast = avalancheRegion => {
+    $.getJSON(`${__API_URL__}avalanche-region-forecast/?format=json&zone=${avalancheRegion}`)
+      .then(avalancheRegion => console.log(avalancheRegion))
+      .catch(error => console.log('error tell us', error));
+  };
+
+  avalancheForecast.getForecast('olympics');
+
+  module.avalancheForecast = avalancheForecast;
+
+})(app);
+
+
+
+
+
+
+// let avalancheRegion = ''
+// function findRegion1(avalancheRegion){
+//     `${__API_URL__}avalanche-region-forecast/?format=json&zone=${avalancheRegion}`
+// }
+
+// let findRegion = ''
+// function findRegion(
+
+// let date = function (
+//     if (res.objects[i].day1_date === "date") {
+//         .then (avalancheDay1Date => arr.push(avalancheDay1Date),
+//         console.log("date")
+//         ).catch(errorCallback);
+// });
+
+// avalancheRegion.fetchOne = (id) => $.getJSON(_API_URL_ + '/api/v1/' + id).catch(errorCallback);
+
+
+// // var = new Date()
