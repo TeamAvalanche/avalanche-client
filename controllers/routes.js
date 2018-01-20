c263f22fa5513fb4030e8518a90c1c401256ef9a
amgranad <ambergkim@gmail.com>
2018-01-20 13:52:37 -0800

    commmit to pull

--- controllers/routes.js
+++ controllers/routes.js
@@ -9,26 +9,26 @@ page('/*', (ctx, next) => {
 });
 
 page('/', () => {
-  app.Location.fetchRegion('olympics').then(info => {
-    console.log('location in route.js ', info);
-    app.homeCreateView.init();
-  }).catch(err => console.log(err));
+    app.Location.fetchRegion('olympics').then(info => {
+        console.log('location in route.js ', info);
+        app.homeCreateView.init();
+    }).catch(err => console.log(err));
 });
 
-page('/weather', () => {
-    $('.page').hide();
-    app.//add stuff
-});
+// page('/weather', () => {
+//     $('.page').hide();
+//     app.//add stuff
+// });
 
-page('/danger', () => {
-    $('.page').hide();
-    app.//add stuff
-});
+// page('/danger', () => {
+//     $('.page').hide();
+//     app.//add stuff
+// });
 
-page('/problems', () => {
-    $('.page').hide();
-    app.//add stuff
-});
+// page('/problems', () => {
+//     $('.page').hide();
+//     app.//add stuff
+// });
 
 page('/feedback', () => {
     $('.page').hide();
