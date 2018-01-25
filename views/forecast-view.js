8eac71783e90e74ec527b299d240a79971795cc7
alicialycan <alicialycan@gmail.com>
2018-01-24 22:40:17 -0800

    add web scraped data, set up new controller, and fix bugs with regions

--- views/forecast-view.js
+++ views/forecast-view.js
@@ -8,21 +8,22 @@ var app = app || {};
 
   const $view = $('#forecast-page');
 
-  forecastCreateView.displayForecast = (locationObject) => {
+  forecastCreateView.displayForecast = (locationObject, temps) => {
     $('#forecast-info').empty();
     let location = locationObject.objects[0].zones[0].zone_name;
     let summary = locationObject.objects[0].bottom_line_summary;
     let aboveDangerLevel = locationObject.objects[0].day1_danger_elev_high;
     let atDangerLevel = locationObject.objects[0].day1_danger_elev_middle;
     let belowDangerLevel = locationObject.objects[0].day1_danger_elev_low2;
-    // let aboveDangerLevel2 = locationObject.objects[0].day1_danger_elev_high2;
-    // let atDangerLevel2 = locationObject.objects[0].day1_danger_elev_middle2;
-    // let belowDangerLevel2 = locationObject.objects[0].day1_danger_elev_low2;
-    let problems = locationObject.objects[0].problems[0].likelihood;
-    console.log(problems, 'problems');
+    let problemType = locationObject.objects[0].problems[0].problem_type.definition;
+
+    let splits = problemType.split('</p>', 1);
+
     $('#forecast-info').append(`
     <h2>Location</h2>
     <p>${location}</p>
+    <h2>Weather</h2>
+    <p>${temps}</p>
     <h2>Bottom Line Summary</h2>
     <p>${summary}</p>
     <div class="triangle-container">
@@ -37,8 +38,7 @@ var app = app || {};
         </div>
     </div>
     <h2>Problems</h2>
-    ${problems}
-
+    <p>${splits}
     `);
   };
 
@@ -50,20 +50,30 @@ var app = app || {};
   //     else if val =
   //   });
 
+  let fetchEverything = (region) => {
+    app.Location.fetchRegion(region, (locationObject) => {
+      //region fetched, now fetch scrape data
+      app.Location.fetchScrape(region, (temps) => {
+        //scrape fetched, now display forecast
+        forecastCreateView.displayForecast(locationObject, temps);
+      });
+    });
+  };
+
   forecastCreateView.init = () => {
     if (localStorage.getItem('region')) {
       let regionString = localStorage.getItem('region');
       let regionParsed = JSON.parse(regionString);
       console.log('region parsed local storage ', regionParsed);
-      app.Location.fetchRegion(regionParsed, forecastCreateView.displayForecast);
+      fetchEverything(regionParsed);
     }
     $view.show();
   };
 
-  $('#forecast-select').on('change', function(e) {
+  $('#forecast-select').on('change', function (e) {
     e.preventDefault();
     let region = $('#forecast-select').val();
-    app.Location.fetchRegion(region, forecastCreateView.displayForecast);
+    fetchEverything(region);
     let regionString = JSON.stringify(region);
     localStorage.setItem('region', regionString);
   });
